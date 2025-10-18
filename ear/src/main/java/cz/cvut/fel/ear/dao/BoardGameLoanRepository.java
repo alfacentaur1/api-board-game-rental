da7444b528308037fc2e030f46a19d91194e9401
Filip Kopecký <filip1kop@gmail.com>
2025-10-18 19:58:38 +0200

    fix/missing-repository-annotation

--- ear/src/main/java/cz/cvut/fel/ear/dao/BoardGameLoanRepository.java
+++ ear/src/main/java/cz/cvut/fel/ear/dao/BoardGameLoanRepository.java
@@ -2,6 +2,8 @@ package cz.cvut.fel.ear.dao;
 
 import cz.cvut.fel.ear.model.BoardGameLoan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardGameLoanRepository extends JpaRepository<BoardGameLoan, Long> {
 }
