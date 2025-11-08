0a449476d5afcad30b54ec598fdb3d974974a2a8
Ondřej Feix <ondra-any@seznam.cz>
2025-11-08 16:58:58 +0100

    added test for ReviewService

--- ear/src/main/java/cz/cvut/fel/ear/service/ReviewService.java
+++ ear/src/main/java/cz/cvut/fel/ear/service/ReviewService.java
@@ -1,83 +1,95 @@
 package cz.cvut.fel.ear.service;
 
-import cz.cvut.fel.ear.dao.BoardGameRepository;
 import cz.cvut.fel.ear.dao.ReviewRepository;
 import cz.cvut.fel.ear.exception.EntityNotFoundException;
+import cz.cvut.fel.ear.exception.InvalidCommentRangeException;
 import cz.cvut.fel.ear.exception.InvalidRatingScoreException;
 import cz.cvut.fel.ear.exception.ParametersException;
 import cz.cvut.fel.ear.model.BoardGame;
 import cz.cvut.fel.ear.model.RegisteredUser;
 import cz.cvut.fel.ear.model.Review;
 import jakarta.transaction.Transactional;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class ReviewService {
-
+    private final UserService userService;
     private final ReviewRepository reviewRepository;
-    private final BoardGameRepository boardGameRepository;
+    private final BoardGameService boardGameService;
     private final int maxRating = 5;
-    private final int minRating = 5;
+    private final int minRating = 0;
     private final int maxCommentRange = 200;
 
-    public ReviewService(ReviewRepository reviewRepository, BoardGameRepository boardGameRepository) {
+    public ReviewService(ReviewRepository reviewRepository, BoardGameService boardGameService, @Lazy UserService userService) {
         this.reviewRepository = reviewRepository;
-        this.boardGameRepository = boardGameRepository;
+        this.boardGameService = boardGameService;
+        this.userService = userService;
     }
 
-    public List<Review> getAllBoardGameReviewsById(int gameId) {
-        BoardGame boardGame = boardGameRepository.getBoardGameById(gameId);
-        if (boardGame == null) {
-            throw new EntityNotFoundException("Board game with id " + gameId + " not found");
-        }
-        return reviewRepository.findAllByBoardGame_IdIn(Collections.singletonList(gameId));
+    public Review findReviewById(long reviewId) {
+        return reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new EntityNotFoundException("Review not found"));
     }
 
+    public List<Review> getReviewsForBoardGame(long gameId) {
+        BoardGame boardGame = boardGameService.getBoardGame(gameId);
+
+        return reviewRepository.findAllByBoardGame_IdIn(List.of(gameId));
+    }
 
     @Transactional
-    public Review createReview(long gameId, String content, int rating, RegisteredUser registeredUser) {
-        BoardGame boardGame = boardGameRepository.getBoardGameById(gameId);
-        if (boardGame == null) {
-            throw new EntityNotFoundException("Board game with id " + gameId + " not found");
-        }
-        if (content == null) {
-            throw new ParametersException("content is null");
-        }
-        if(registeredUser == null ){
-            throw new ParametersException("registered user is null");
-        }
+    public Review createReview(long userId, long gameId, String content, int ratingValue) {
+        // Find the user
+        RegisteredUser user = userService.findById(userId);
 
-        if (rating > maxRating || rating < minRating) {
-            throw new InvalidRatingScoreException("Rating must be between " + minRating + " and " + maxRating);
-        }
+        // Find the board game
+        BoardGame boardGame = boardGameService.getBoardGame(gameId);
 
-        if (content.length() > maxCommentRange) {
-            throw new InvalidRatingScoreException("Review content is limited to " + maxCommentRange + " characters");
-        }
 
-        Review review = new Review();
-        review.setBoardGame(boardGame);
-        review.setComment(content);
-        review.setScore(rating);
-        review.setCreatedAt(LocalDateTime.now());
-        review.setAuthor(registeredUser);
+        validateReviewInput(content, ratingValue);
 
-        reviewRepository.save(review);
-        return review;
+        // Create new review
+        Review newReview = new Review();
+        newReview.setBoardGame(boardGame);
+        newReview.setComment(content);
+        newReview.setScore(ratingValue);
+        newReview.setCreatedAt(LocalDateTime.now());
+        newReview.setAuthor(user);
+
+        reviewRepository.save(newReview);
+
+        // link review to user
+        userService.linkReviewToUser(userId, newReview.getId());
+
+        return newReview;
     }
 
+    public void deleteReview(long reviewId) {
+        Review review = findReviewById(reviewId);
 
-    public void deleteReview(long id) {
-        Review review = reviewRepository.findById(id).orElse(null);
-        if (review == null) {
-            throw new EntityNotFoundException("Review with id " + id + " not found");
-        }
         reviewRepository.delete(review);
-
     }
 
+
+    private void validateReviewInput(String content, int ratingValue) {
+        // Validate content
+        if (content == null) {
+            throw new ParametersException("Content is null");
+        } else if (content.length() > maxCommentRange) {
+            throw new InvalidCommentRangeException(
+                    String.format("Review content is limited to %d current : %d", maxCommentRange, content.length())
+            );
+        }
+
+        // Validate Rating value
+        if (ratingValue > maxRating || ratingValue < minRating) {
+            throw new InvalidRatingScoreException(
+                    String.format("Rating must be between %d and %d current : %d", maxCommentRange, minRating, ratingValue)
+            );
+        }
+    }
 }
