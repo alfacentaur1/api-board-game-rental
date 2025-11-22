a5fd87a80bafc07db95ac10e95dabd47e872de07
Filip <115399354+alfacentaur1@users.noreply.github.com>
2025-11-22 09:05:07 +0100

    JSON docs for importing to postman

--- ear/src/main/java/cz/cvut/fel/ear/BoardGameRentalApplication.java
+++ ear/src/main/java/cz/cvut/fel/ear/BoardGameRentalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseworkApplication {
+public class BoardGameRentalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseworkApplication.class, args);
+		SpringApplication.run(BoardGameRentalApplication.class, args);
 	}
 
 }
