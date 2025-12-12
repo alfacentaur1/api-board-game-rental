112f8f3b954f905ab1988290887be9b500969762
Filip <115399354+alfacentaur1@users.noreply.github.com>
2025-12-12 14:38:13 +0100

    error fixes

--- ear/src/main/java/cz/cvut/fel/ear/controller/handler/GlobalExceptionHandler.java
+++ ear/src/main/java/cz/cvut/fel/ear/controller/handler/GlobalExceptionHandler.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.Map;
