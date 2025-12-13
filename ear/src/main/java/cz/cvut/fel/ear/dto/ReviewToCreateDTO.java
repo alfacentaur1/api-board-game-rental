44308c7035ca8a6e6e375421c4ce2f1852a63f1c
Ondřej Feix <ondra-any@seznam.cz>
2025-12-13 18:56:41 +0100

    modified to get user context from request and not DTO

--- ear/src/main/java/cz/cvut/fel/ear/dto/ReviewToCreateDTO.java
+++ ear/src/main/java/cz/cvut/fel/ear/dto/ReviewToCreateDTO.java
@@ -3,9 +3,8 @@ package cz.cvut.fel.ear.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record ReviewToCreateDTO(
-        @NotNull Long userId,
         @NotNull Long gameId,
         String content,
-        int score
+        @NotNull int score
 ) implements BasicDTO {
 }
